594fad0286aca94b9be63b95a859360f536b954e
Jason Inouye <jasonjinouye@gmail.com>
2022-04-17 10:12:39 -0500

    added breadcrumbs and buttons from react

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Button, Alert, Breadcrumb, BreadcrumbItem } from 'reactstrap'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Breadcrumb>
+          <BreadcrumbItem>Test</BreadcrumbItem>
+          <BreadcrumbItem>Test 2</BreadcrumbItem>
+          <BreadcrumbItem active>Test 3</BreadcrumbItem>
+        </Breadcrumb>
+        <Alert variant="success">This is a button</Alert>
+        <Button color="danger">Test Button</Button>
       </header>
     </div>
   );
